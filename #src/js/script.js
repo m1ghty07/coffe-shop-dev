2a6271f49064f75b86f091708ca581fff285c493
m1ghty07 <dulchenko_s@ukr.net>
2021-12-02 18:07:38 +0200

    finish

--- #src/js/script.js
+++ #src/js/script.js
@@ -3,38 +3,49 @@ function testWebP(callback) {
   webP.onload = webP.onerror = function () {
     callback(webP.height === 2);
   };
-  webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+  webP.src =
+    "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
 }
 
 testWebP(function (support) {
-
   if (support === true) {
-    document.querySelector('body').classList.add('webp');
+    document.querySelector("body").classList.add("webp");
   } else {
-    document.querySelector('body').classList.add('no-webp');
+    document.querySelector("body").classList.add("no-webp");
   }
 });
 
-window.addEventListener('DOMContentLoaded', () => {
-  'use strict';
+window.addEventListener("DOMContentLoaded", () => {
+  "use strict";
 
-  const burger = document.querySelector('.burger'),
-    menuList = document.querySelector('.menu__list'),
-    btn = document.querySelector('.burger span'),
-    menuLinks = document.querySelectorAll('.menu__link');
+  const burger = document.querySelector(".burger"),
+    menuList = document.querySelector(".menu__list"),
+    btn = document.querySelector(".burger span"),
+    menuLinks = document.querySelectorAll(".menu__link");
 
-  burger.addEventListener('click', () => {
-    menuList.classList.toggle('active');
-    btn.classList.toggle('active');
+  burger.addEventListener("click", () => {
+    menuList.classList.toggle("active");
+    btn.classList.toggle("active");
+    document.body.classList.toggle("hidden");
   });
 
+  function itemFilter() {
+    const tabs = document.querySelectorAll(".tab"),
+      cards = document.querySelectorAll(".market__item ");
 
-  menuLinks.forEach(item => {
-    item.addEventListener('click', (e) => {
-      if (e.target === item && !menuList.classList.contains('active')) {
-        e.preventDefault();
-      }
+    tabs.forEach((tab) => {
+      tab.addEventListener("click", (e) => {
+        let tabAttr = tab.getAttribute("data-country");
+
+        cards.forEach((card) => {
+          if (!card.classList.contains(tabAttr)) {
+            card.classList.add("hide");
+          } else {
+            card.classList.remove("hide");
+          }
+        });
+      });
     });
-  });
-
+  }
+  itemFilter();
 });
